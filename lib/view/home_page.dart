747c668fdba8deed9347b69f6f879f56b2b4263c
arcanjo96 <lucasarcanjo.ld@gmail.com>
2020-07-26 19:20:21 -0300

    Added home page.

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -6,12 +6,53 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var content = Column();
-
-    return Layout.render(
-      context,
-      content,
-      bottomItemSelected: 0
+    var content = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          decoration: BoxDecoration(
+            color: Layout.light(),
+            borderRadius: BorderRadius.circular(25),
+          ),
+          margin: EdgeInsets.only(left: 20, right: 20),
+          padding: EdgeInsets.all(20),
+          height: 100,
+          width: double.infinity,
+          child: Text('Promoções'),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Expanded(
+          child: Container(
+            decoration: BoxDecoration(
+              color: Layout.light(0.7),
+              borderRadius: BorderRadius.circular(25),
+            ),
+            margin: EdgeInsets.only(left: 20, right: 20),
+            padding: EdgeInsets.all(20),
+            width: double.infinity,
+            child: Text('Produtos'),
+          ),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Padding(
+          padding: const EdgeInsets.only(left: 30.0),
+          child: Text(
+            'Categorias',
+            style: Theme.of(context).textTheme.headline6.copyWith(
+                  color: Layout.light(),
+                ),
+          ),
+        ),
+        Container(
+          height: 90,
+        ),
+      ],
     );
+
+    return Layout.render(context, content, bottomItemSelected: 0);
   }
 }
